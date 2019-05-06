917c31fad7691f3cc1502256cba5f3c003b9697e
eaNODE <ea_pain@yahoo.com>
2019-05-06 16:57:10 +0800

    bug

--- server/middleware/minio.js
+++ server/middleware/minio.js
@@ -1,14 +1,5 @@
 var Minio = require('minio');
 
-<<<<<<< HEAD
-var minioClient = new Minio.Client({
-    endPoint: 'localhost',
-    port: 9000,
-    useSSL: false,
-    accessKey: 'XXT3LZZZ3HQN6U3AIBB8',
-    secretKey: '4GQmWzbPWPsYXCtbby4+z65rrtvjXvo3+2lgM1FQ'
-});
-=======
 // options :
 // {
 //     endPoint: 'play.minio.io',
@@ -22,7 +13,6 @@ function makeMinio (options) {
 }
 
 var minios = [];
->>>>>>> d836b50c7105d713d36320b49c39ef84c618447f
 
 module.exports = function middleware(req, res, next) {
     req.minio = {
