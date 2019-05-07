d836b50c7105d713d36320b49c39ef84c618447f
force1267 <force1267@gmail.com>
2019-05-07 16:54:17 +0430

    minio mw

--- server/middleware/minio.js
+++ server/middleware/minio.js
@@ -1,14 +1,23 @@
 var Minio = require('minio');
 
-var minioClient = new Minio.Client({
-    endPoint: 'play.minio.io',
-    port: 9000,
-    useSSL: true,
-    accessKey: 'Q3AM3UQ867SPQQA43P2F',
-    secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
-});
+// options :
+// {
+//     endPoint: 'play.minio.io',
+//     port: 9000,
+//     useSSL: true,
+//     accessKey: 'Q3AM3UQ867SPQQA43P2F',
+//     secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
+// }
+function makeMinio (options) {
+    return minios.push(new Minio.Client(options));
+}
+
+var minios = [];
 
 module.exports = function middleware(req, res, next) {
-    req.minio = minioClient;
+    req.minio = {
+        minios,
+        add: makeMinio
+    };
     next();
 }
