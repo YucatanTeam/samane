acf3d1650c1d5e09e7dc44061c610ddd2378eae1
force1267 <force1267@gmail.com>
2019-05-07 16:41:47 +0430

    mids

---
+++ server/middleware/minio.js
@@ -0,0 +1,14 @@
+var Minio = require('minio');
+
+var minioClient = new Minio.Client({
+    endPoint: 'play.minio.io',
+    port: 9000,
+    useSSL: true,
+    accessKey: 'Q3AM3UQ867SPQQA43P2F',
+    secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
+});
+
+module.exports = function middleware(req, res, next) {
+    req.minio = minioClient;
+    next();
+}
