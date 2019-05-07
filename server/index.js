86c18ed7a2e8789f66969bd4b797f1d4ade1355f
eaNODE <ea_pain@yahoo.com>
2019-05-07 16:00:14 +0800

    working on auth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require("cookie-session");
+const helmet = require('helmet');
 const api = express();
 
+
+// CORS setup
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, x-access-token')
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200)
+    }
+    else {
+      next()
+    }
+})
+
+app.use(helmet())
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieSession({
+    name: "session",
+    keys: [
+        process.env.SECRET || require('crypto').randomBytes(32).hexSlice()
+    ],
+    maxAge: 1 * 60 * 60 * 1000, // 24 hrs
+}));
+
 
 api.use(require("./middleware/minio.js"));
 api.use(require("./middleware/db.js"));
