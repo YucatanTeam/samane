6de1db89910a6a176a20d8403f3357e5c13ecf87
khashayar1375 <khashayar.mafi75@gmail.com>
2019-05-15 19:15:28 +0430

    doc more edit

--- server/route/doc.js
+++ server/route/doc.js
@@ -6,4 +6,22 @@ var doc = app.Router();
 doc.get("/all",function(req,res){
     res.send(`access granted for ${req.user.username} to all docs`);
 });
+doc.post("/doc/add",function(req,res){
+    var re
+});
+doc.post("/more/:id/edit",function(req,res){
+    var body = JSON.stringify(req.body)
+    var moreAll = req.sql.query("SELECT * FROM doc WHERE id=? ", [req.params.id],function(err,doc){
+        try {
+            console.log(doc)
+            res.send("200");
+        } catch (err) {
+            res.send(err)
+        }
+      
+    })
+    
+    
+});
+
 module.exports= doc;
