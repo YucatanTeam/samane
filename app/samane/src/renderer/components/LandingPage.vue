5dd3b205958e62f152c03bc501eb30a703b11222
eaNODE <ea_pain@yahoo.com>
2019-06-26 18:10:58 +0800

    added materialize but not use in index.ejs

--- app/samane/src/renderer/components/LandingPage.vue
+++ app/samane/src/renderer/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Welcome to your new project!
+          Welcome to samane
         </span>
         <system-information></system-information>
       </div>
