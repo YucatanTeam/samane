7c2dca8f098ba59f660e436b639c513be7e42e53
Camel-Black <khashayar.mafi75@gmail.com>
2019-06-30 07:52:38 +0430

    html

---
+++ app/main.js
@@ -0,0 +1,36 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+require('electron-reload')(__dirname, {
+    // Note that the path to electron may vary according to the main file
+    electron: require(`${__dirname}/node_modules/electron`)})
+let mainWindow
+
+function createWindow () {
+
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 1000,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+ 
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+
+  if (mainWindow === null) createWindow()
+})
